9191e9b9daa83141274baebb647064ca497f1021
clnt <c.connery14@gmail.com>
2024-10-03 22:32:10 +0100

    route fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -4,7 +4,6 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
-// webhook secret
 export async function POST(req: Request) {
   try {
     console.log("Loading environment variables...");
